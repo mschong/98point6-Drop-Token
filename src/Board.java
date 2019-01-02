356afb80a26665398fa92642512e5b111baea745
mschong <marinachong96@gmail.com>
2019-01-02 13:39:23 -0700

    Added Board and Game class

---
+++ src/Board.java
@@ -0,0 +1,131 @@
+import java.util.ArrayList;
+
+
+public class Board {
+	private static ArrayList<Place> grid;
+	private static final int SIZE = 4;
+	private static int player;
+
+	
+	public int getSize() {
+		return SIZE;
+	}
+	
+	/*Create empty board with zeroes*/
+	public void initializeGrid() {
+		player = 1;
+		grid = new ArrayList<>();
+		for (int i = 1; i <= SIZE; i++) {
+			for (int j = 1; j <= SIZE; j++) {
+				grid.add(new Place(j, i));
+			}
+		}
+	}
+	
+	/**
+	 * Display board
+	 */
+	public void printBoard() {
+		for (int i = SIZE; i >= 1; i--) {
+			System.out.print("| ");
+			for (int j = 1; j <= SIZE; j++) {
+				System.out.print(getPlace(j, i).getPlayer() + " ");
+			}
+			System.out.println();
+		}
+		System.out.print("+");
+		for (int i = 0; i < SIZE; i++) {
+			System.out.print("--");
+		}
+		System.out.print("\n  ");
+		for (int i = 1; i <= SIZE; i++) {
+			System.out.print(i + " ");
+		}
+	}
+
+
+	/**
+	 * Takes x and y coordinates and returns the place corresponding to those coordinates
+	 * @param x horizontal coordinate
+	 * @param y vertical coordinate
+	 * @return the Place at xy
+	 */
+	public Place getPlace(int x, int y) {
+		for (Place p : grid) {
+			if (p.getX() == x && p.getY() == y)
+				return p;
+		}
+		return null;
+	}
+	
+	/*Adds token in the board in the specified column*/
+	public String addToken(int column) {
+		for (int i = 1; i <= SIZE; i++) {
+			if (getPlace(column, i).getPlayer() == 0) {
+				getPlace(column, i).setPlayer(player);
+				//Checks if it's a win after the token was added
+				if (isWin(column,i))
+					return "WIN";
+				//If it was not a win it checks if it ended in a draw
+				else if (isDraw())
+					return "DRAW";
+				//Changes the turn to the next player
+				else {
+					if (player == 1)
+						player = 2;
+					else
+						player = 1;
+					return "OK";
+				}
+			}
+		}
+		return "ERROR";
+	}
+	
+	public boolean isCorner(int x, int y) {
+		if((x==1&&y==1) || (x==1&&y==SIZE) || (x==SIZE&&y==1) || (x==SIZE&&y==SIZE)){
+			return true;
+		}
+		return false;
+	}
+	
+	/*Checks if a player has won*/
+	public boolean isWin(int x, int y) {
+		if(isCorner(x,y))
+			if(horizontalWin(y) || verticalWin(x) || diagonalWin(x,y))
+				return true;
+		else
+			if(horizontalWin(y) || verticalWin(x))
+				return true;
+		return false;
+	}
+	/*Checks the row for the same player*/
+	public boolean horizontalWin(int y) {
+		for(int i = 1; i <= SIZE; i++) {
+			if(getPlace(i, y).getPlayer() != player)
+				return false;
+		}
+		return true;
+	}
+	
+	/*Checks column for the same player*/
+	public boolean verticalWin(int x) {
+		for(int i = 1; i <= SIZE; i++) {
+			if(getPlace(x, i).getPlayer() != player)
+				return false;
+		}
+		return true;
+	}
+	
+	public boolean diagonalWin(int x, int y) {
+		return false;
+	}
+	
+	public boolean isDraw() {
+		for(Place p : grid) {
+			if(p.getPlayer() == 0)
+				return false;
+		}
+		return true;
+	}
+}
---
+++ src/Game.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Game {
+	private static Board board = new Board();
+	
+	
+	/*Takes the user input and what the user wants to do*/
+	public static void play() {
+		Scanner scr = new Scanner(System.in);
+		String state = "";
+		while (!(state.equals("WIN") || state.equals("DRAW"))) {
+			System.out.print("\n> ");
+			String line = scr.nextLine();
+			String[] cmd = line.split(" ");
+			if (cmd[0].equals("BOARD"))
+				board.printBoard();
+			else if (cmd[0].equals("GET"))
+				System.out.println("hacer algo para get");
+			else if (cmd[0].equals("PUT") && cmd.length > 1) {
+				int col = Integer.parseInt(cmd[1]);
+				if (col > board.getSize() || col < 1)
+					System.out.println("ERROR");
+				else {
+					state = board.addToken(col);
+					System.out.println(state);
+				}
+			}
+			else if(cmd[0].equals("EXIT"))
+				System.exit(0);
+			else
+				System.out.println("INVALID COMMAND");
+		}
+		board.printBoard();
+		scr.close();
+	}
+
+	
+
+	public static void main(String[] args) {
+		System.out.println("98point6 DROP TOKEN");
+		System.out.println("-----------------------------------------------------------------------");
+		System.out.println("| • PUT <column>   -> (OK|ERROR|WIN|DRAW)                               |");
+		System.out.println("| • GET            -> List of columns that have been succesfully put on |");
+		System.out.println("| • BOARD          -> a 4x4 matrix that shows the board state           |");
+		System.out.println("| • EXIT           -> Quit game                                         |");
+		System.out.println("-----------------------------------------------------------------------");
+		board.initializeGrid();
+		play();
+	}
+}
