ecacc9dc6eb2ce369fee82fe94cafbd1148252df
mschong <marinachong96@gmail.com>
2019-01-02 13:04:00 -0700

    Initial commit - Added Place class

---
+++ src/Place.java
@@ -0,0 +1,40 @@
+
+
+public class Place {
+	private int x;
+	private int y;
+	private int player;
+	
+	public Place(int x, int y) {
+		this.x = x;
+		this.y = y;
+		this.player = 0;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getPlayer() {
+		return player;
+	}
+
+	public void setPlayer(int player) {
+		this.player = player;
+	}
+	
+	
+	
+}
