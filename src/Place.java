ce5ed15c831be1d60eaa3695e66b83c1351e2a93
mschong <marinachong96@gmail.com>
2019-04-29 12:00:00 +0100

    Added documentation

--- src/Place.java
+++ src/Place.java
@@ -1,40 +1,63 @@
 
-
 public class Place {
+	/** Horizontal coordinate (column number) */
 	private int x;
+
+	/** Vertical coordinate (row number) */
 	private int y;
+
+	/** Player number token */
 	private int player;
-	
+
+	/**
+	 * Creates an empty place in the given coordinates
+	 * 
+	 * @param x
+	 *            x coordinate
+	 * @param y
+	 *            y coordinate
+	 */
 	public Place(int x, int y) {
 		this.x = x;
 		this.y = y;
 		this.player = 0;
 	}
 
+	/**
+	 * Returns x coordinate
+	 * 
+	 * @return x coordinate
+	 */
 	public int getX() {
 		return x;
 	}
 
-	public void setX(int x) {
-		this.x = x;
-	}
-
+	/**
+	 * Returns y coordinate
+	 * 
+	 * @return y coordinate
+	 */
 	public int getY() {
 		return y;
 	}
 
-	public void setY(int y) {
-		this.y = y;
-	}
-
+	/**
+	 * Returns player number
+	 * 
+	 * @return player number
+	 */
 	public int getPlayer() {
 		return player;
 	}
 
+	/**
+	 * Adds player token
+	 * 
+	 * @param player
+	 *            player number
+	 */
 	public void setPlayer(int player) {
 		this.player = player;
 	}
-	
-	
-	
+
 }
